5e55daa75f6611605df7d13df73e6d2f4f9af3c9
rioaldie <rioaldierwanto@gmail.com>
2023-11-18 07:25:56 +0700

    add section title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,14 @@ const Home = () => {
     <>
       <section className="px-[3.25%] w-full h-screen tab:h-full tab:pt-[4rem] mt-14 ">
         <Banner />
-        <div className="w-full border border-black mt-14"></div>
+        <div className="w-full  mt-14 flex flex-row justify-between px-36">
+          <p className="text-xl font-semibold text-black">
+            Capaian Harianmu
+          </p>
+          <p className="text-xl font-semibold text-orange">
+            Lihat semua
+          </p>
+        </div>
       </section>
     </>
   );
