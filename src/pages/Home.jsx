392b4e0bbd7d187fd9c589e88c087e98750a705e
rioaldie <rioaldierwanto@gmail.com>
2023-11-19 20:05:24 +0700

    make a footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import TrackingContainer from '../components/containers/TrackingContainer';
 const Home = () => {
   return (
     <>
-      <section className="px-[12.25%] w-full h-screen tab:h-full tab:pt-[4rem] mt-14 ">
+      <section className="px-[12.25%] w-full  tab:h-full tab:pt-[4rem] mt-14 ">
         <Banner />
         <div className="w-full  mt-14 flex flex-row justify-between ">
           <p className="text-l font-semibold text-black">
@@ -20,6 +20,29 @@ const Home = () => {
         <FilterContainer />
         <ListFoodContainer />
       </section>
+      <footer className="w-full h-64 flex flex-col bg-navy justify-between  items-center">
+        <div className="flex w-full flex-row justify-between px-10 py-12">
+          <img src="icons/nutrify-logo-white.svg" alt="" />
+          <div className="text-white flex flex-row gap-4 items-center justify-center">
+            <a href="#">Home</a>
+            <span className="w-2 h-2 bg-white rounded-full"></span>
+            <a href="#">Favorite</a>
+            <span className="w-2 h-2 bg-white rounded-full"></span>
+            <a href="#">About Us</a>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-3">
+            <p className="text-white  ">Find us :</p>
+            <img src="/icons/facebook.svg" alt="" />
+            <img src="/icons/instagram.svg" alt="" />
+            <img src="/icons/x-twitter.svg" alt="" />
+            <img src="/icons/envelope-regular.svg" alt="" />
+          </div>
+        </div>
+        <div className="h-16 w-full bg-darknavy flex flex-row justify-between  px-12 items-center text-white-400">
+          <p>2023@ All rights reserved. Design by Nutrify team’s </p>
+          <p>Terms & Condition</p>
+        </div>
+      </footer>
     </>
   );
 };
