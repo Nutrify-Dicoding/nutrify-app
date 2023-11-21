5da3e15d6a90584e0a0317c7c07addefdc9e5ef8
agustinrahmawati03 <rhmagustin08@gmail.com>
2023-11-21 22:30:22 +0700

    change name

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
 		<div className="w-full h-[300px] tab:h-[250px] sm:h-[200px] pl-10 sm:pl-5 md:mt-32 mt-36 flex items-center bg-[url('images/banner-nutrify.png')] bg-no-repeat bg-center bg-cover rounded-[20px]">
 			<div className="">
 				<p className="text-4xl font-bold text-white sm:text-mobile-4xl">
-					Hello <span className="text-orange">Sabrina</span>{' '}
+					Hello <span className="text-orange">Agustin</span>{' '}
 				</p>
 				<p className="text-lg text-white-200">Jangan lupa penuhi nutrisi harianmu ya!</p>
 			</div>
