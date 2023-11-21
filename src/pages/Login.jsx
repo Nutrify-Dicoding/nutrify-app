ea83509f67bdb729b2fee2caae71019b2d391919
agustinrahmawati03 <rhmagustin08@gmail.com>
2023-11-22 00:52:07 +0700

    Style (login): change style on login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,14 +7,17 @@ const Login = () => {
         <div style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-center h-screen w-screen fixed top-0 left-0 z-[-2]"></div>
         <div className="flex items-center justify-center mt-[10rem] z-[2]">
         <div className="flex">           
-        <img src={login1} alt="background" className="w-[320px] h-[340px]" />
-        <div className="container max-w-sm bg-white bg-opacity-5 p-10 rounded-md shadow-lg">
-        <h1 className="text-3xl font-thin text-center">Form Login</h1>
-        <form action="" className="mt-5">
-            <input type="text" className="block w-full mt-3 border border-sky-400 rounded-md h-8 p-2 placeholder-[#002140] text-sm" placeholder="Email"/>
-            <input type="password" className="block w-full mt-3 border border-sky-400 rounded-md h-8 p-2 placeholder-[#002140] text-sm" placeholder="Password"/>
-            <button className="mt-3 text-sm w-full bg-sky-600 border border-1 px-2 py-1 h-8 rounded-md text-white">Login</button>
+        <img src={login1} alt="background" className="w-[250px] h-[270px]" />
+        <div className="container max-w-sm bg-white bg-opacity-5 backdrop-blur-sm p-10 rounded-md shadow-lg">
+        {/* <h1 className="text-3xl font-thin text-center">Form Login</h1> */}
+        <span className="text-white mr-[4px] font-bold" >Login</span>
+        <span className="text-[#FB8500] font-bold" >Now!</span>
+        <form action="" className="mt-1">
+            <input type="text" className="block w-full mt-3 rounded-md h-9 p-3 placeholder-[#002140] text-xs" placeholder="Email"/>
+            <input type="password" className="block w-full mt-3 rounded-md h-9 p-3 placeholder-[#002140] text-xs" placeholder="Password"/>
+            <button className="mt-3 text-xs w-full bg-[#FB8500] px-2 py-1 h-8 rounded-md text-white">Login</button>
         </form>
+        <p className="mt-3 text-xs text-white">Belum punya akun? <a href="#" className="text-[#FB8500] font-bold">Register</a></p>
         </div>
         </div>
         </div>  
