16c1ee09e14e72713c964d0515532889ed5c411a
agustinrahmawati03 <rhmagustin08@gmail.com>
2023-11-22 21:08:18 +0700

    test signup

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import background from "../assets/background.png"
+
+const SignUp = () => {
+    return (
+        <>
+         <div style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-center h-screen w-screen fixed top-0 left-0 z-[-2]"></div>
+        </>
+        )
+    }
+    
+    export default SignUp;
