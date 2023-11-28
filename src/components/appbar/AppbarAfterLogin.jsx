752783cb092aaf13ae17d5b839cc38d23f35595d
Ridho Ahmad Fauzi <ridho.ahmad21@students.unila.ac.id>
2023-11-28 15:29:19 +0700

    Edit appbar before login and card food

--- src/components/appbar/AppbarAfterLogin.jsx
+++ src/components/appbar/AppbarAfterLogin.jsx
@@ -1,20 +1,20 @@
-import { Link, useNavigate } from "react-router-dom";
-import SearchInputBar from "../SearchInputBar";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { clearAuth } from "../../redux/slices/authSlice";
+import { Link, useNavigate } from 'react-router-dom';
+import SearchInputBar from '../SearchInputBar';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearAuth } from '../../redux/slices/authSlice';
 
 const AppbarAfterLogin = () => {
-	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 	const [isOpen, setIsOpen] = useState(false);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	useEffect(() => {
-		if (!isAuthenticated) navigate('/auth/signin')
-	}, [isAuthenticated, navigate])
+		if (!isAuthenticated) navigate('/auth/signin');
+	}, [isAuthenticated, navigate]);
 	const logout = () => {
 		dispatch(clearAuth());
-	}
+	};
 	return (
 		<>
 			<nav className="bg-white border-gray-200 border-b-[1px] w-full px-[6.25%] fixed top-0 z-10">
@@ -24,8 +24,12 @@ const AppbarAfterLogin = () => {
 					</Link>
 					<SearchInputBar></SearchInputBar>
 					<div className="flex flex-row gap-5 items-center ">
-						<Link to={"/"} className="flex sm:hidden cursor-pointer text-base">Home</Link>
-						<Link to={"/track"} className="flex sm:hidden cursor-pointer text-base">Tracking</Link>
+						<Link to={'/'} className="flex sm:hidden cursor-pointer text-base">
+							Home
+						</Link>
+						<Link to={'/track'} className="flex sm:hidden cursor-pointer text-base">
+							Tracking
+						</Link>
 						<button
 							type="button"
 							className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -37,12 +41,22 @@ const AppbarAfterLogin = () => {
 							<img className="w-8 h-8 rounded-full" src="/images/user-nutrify.png" alt="user photo" />
 						</button>
 						<div>
-							<img src="/icons/drop-icon.svg" className={`cursor-pointer transition ${isOpen?'rotate-180':''}`} onClick={() => { setIsOpen(!isOpen) }} />
+							<img
+								src="/icons/drop-icon.svg"
+								className={`cursor-pointer transition ${isOpen ? 'rotate-180' : ''}`}
+								onClick={() => {
+									setIsOpen(!isOpen);
+								}}
+							/>
 							{/* sementara */}
 							<div className={`absolute ${isOpen ? 'block' : 'hidden'}`}>
 								<ul className="mt-4 ms-[-50px] lg:ms-[-80px] bg-white">
-									<li className="py-2 px-5 border"><Link to={"/profile"}>Profile</Link></li>
-									<li className="py-2 px-5 border" role="button" onClick={()=>logout()}>Logout</li>
+									<li className="py-2 px-5 border">
+										<Link to={'/profile'}>Profile</Link>
+									</li>
+									<li className="py-2 px-5 border" role="button" onClick={() => logout()}>
+										Logout
+									</li>
 								</ul>
 							</div>
 						</div>
--- src/components/cards/CardFood.jsx
+++ src/components/cards/CardFood.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 /* eslint-disable react/prop-types */
 const CardFood = ({ id, name, description, fat, calorie, protein, carbohydrate, image }) => {
 	return (
-		<Link to={`/foods/${id}`} className="col-span-1 mx-auto bg-white rounded-lg shadow-[0px_0px_44px_-5px_rgba(0,0,0,0.10)]">
-			<img className="rounded-t-lg w-full max-h-[150px]" src={image||'https://picsum.photos/265/150'} alt={name} loading="lazy"/>
+		<Link to={`/foods/${id}`} className="col-span-1 mx-auto w-full bg-white rounded-lg shadow-[0px_0px_44px_-5px_rgba(0,0,0,0.10)]">
+			<img
+				className="rounded-t-lg w-full max-h-[150px] object-cover"
+				src={image || 'https://picsum.photos/265/150'}
+				alt={name}
+				loading="lazy"
+			/>
 			<div className="pt-5 pb-7 px-[14px] sm:px-[10px] ">
 				<h3 className="text-xl sm:text-mobile-xl font-semibold text-navy">{name}</h3>
 				<p className="text sm:text-sm text-navy opacity-80">{description}</p>
-				<div className="w-full flex flex-row gap-5 justify-around mt-4 sm:gap-4">
+				<div className="w-full grid grid-cols-4 sm:grid-cols-2 mt-4 sm:gap-4">
 					<div className="text-xs flex justify-center gap-1 items-center text-navy">
 						<img src="/icons/cloud-meatball-solid.svg" className="inline h-[18px] sm:h-4" /> {fat}
 					</div>
