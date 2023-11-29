55b821f3c25e9e80085ee71945f37b5eec7b553d
pausii <1121130019@global.ac.id>
2023-11-29 23:26:22 +0700

    Edit Profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from 'react-router-dom';
 const Profile = () => {
 	const token = useSelector((state) => state.auth.token)
 	const [user, setUser] = useState({});
+	const [isFetching, setIsFetching] = useState(true);
 	const navigate = useNavigate();
 	useEffect(() => {
 		const config = {
@@ -20,13 +21,16 @@ const Profile = () => {
 			.catch((err) => {
 				if(err.response.status === 401) navigate('/auth/signin')
 				else console.log(err);
-			});
+			})
+			.finally(() => {
+				setIsFetching(false);
+			})
 	}, [token, navigate]);
 	return (
 		<section className="pt-24 px-[6.25%] text-navy transition-all mb-20 sm:mb-12">
 			<div className="border p-4 rounded-lg mt-2 flex items-center md:items-start">
 				<div>
-					<img className="rounded-full me-4 w-[70px] h-[70px]" src="https://picsum.photos/70/70" alt="User Profile" />
+					<img className="rounded-full me-4 w-[70px] h-[70px]" src="/icons/user.png" alt="User Profile" />
 				</div>
 				<div className="flex md:block justify-between w-full">
 					<div className="ms-3">
@@ -50,27 +54,27 @@ const Profile = () => {
 				<div className="grid grid-cols-3 sm:grid-cols-1">
 					<div>
 						<div className="font-medium mt-5">Email</div>
-						<div className="text-sm">{user.email}</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.email}</div>
 					</div>
 					<div>
 						<div className="font-medium mt-5">Jenis Kelamin</div>
-						<div className="text-sm">{user.gender}</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.gender}</div>
 					</div>
 					<div>
 						<div className="font-medium mt-5">Umur</div>
-						<div className="text-sm">{user.umur} Tahun</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.umur} Tahun</div>
 					</div>
 					<div>
 						<div className="font-medium mt-5">Berat Badan</div>
-						<div className="text-sm">{user.berat} Kilogram</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.berat} Kilogram</div>
 					</div>
 					<div>
 						<div className="font-medium mt-5">Tinggi Badan</div>
-						<div className="text-sm">{user.tinggi} Centimeter</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.tinggi} Centimeter</div>
 					</div>
 					<div>
 						<div className="font-medium mt-5">Tingkat Kegiatan</div>
-						<div className="text-sm">{user.levelAktivitas ? user.levelAktivitas.ket : ''}</div>
+						<div className={`text-sm ${isFetching?'animate-pulse bg-white-400 h-6 rounded-lg me-5':''}`}>{user.levelAktivitas ? user.levelAktivitas.ket : ''}</div>
 					</div>
 				</div>
 			</div>
