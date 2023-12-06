887ca9f3e61a2a24b32504bbb497ad2a93cecae6
pausii <1121130019@global.ac.id>
2023-12-06 21:11:39 +0700

    Integrate Recommendation component with API

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Button from './buttons/Button';
 import CardFood from './cards/CardFood';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import PlaceholderCardFood from './placeholder/PlaceholderCardFood';
 
 const Recommendation = () => {
+	const [foods, setFoods] = useState([]);
+	const [isFetching, setIsFetching] = useState(false);
+	const token = useSelector((state) => state.auth.token)
+	useEffect(() => {
+		setIsFetching(true);
+		const config = {
+			headers: {
+				'Authorization': `Bearer ${token}`,
+			},
+		};
+		axios.get('/foods', config).then((res) => {
+			if(res.status === 200) setFoods(res.data.food)
+		}).finally(() => {
+			setIsFetching(false);
+		});
+	}, [token]);
 	return (
 		<div className="mt-12 mb-20">
-			<p
-				className="font-semibold text-2xl text-navy
-        "
-			>
+			<p className="font-semibold text-2xl text-navy">
 				Makanan Terpopuler
 			</p>
-			<div className="grid mt-8 sm:grid-cols-1 tab:grid-cols-3 grid-cols-4 gap-5 mb-12 ">
-				{Array.from({ length: 4 }, (_, index) => (
-					<CardFood key={index} name="Nama Makanan" description="Description..." fat="93" calorie="100" protein="80" carbohydrate="100" />
-				))}
+			<div className="grid mt-8 sm:grid-cols-1 tab:grid-cols-3 grid-cols-4 gap-5 mb-12">
+				{foods.map((item, index) => {
+					return <CardFood key={index} id={item._id} name={item.name} description={item.desc} fat={item.fat} calorie={item.cal} protein={item.protein} carbohydrate={item.carb} image={item.image.includes('http') ? item.image : 'https://picsum.photos/265/150'} />;
+				})}
+
+				{/* While the data is being retrieved */}
+				{isFetching &&
+					[...Array(4)].map((_, i) => {
+						return (
+							<PlaceholderCardFood key={i} />
+						);
+					})}
 			</div>
 			<div className="w-full flex justify-center">
 				<Link to="/">
