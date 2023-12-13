af188c9f4a57d358b5cc9211bcddff770684ddb5
Ridho Ahmad Fauzi <ridho.ahmad21@students.unila.ac.id>
2023-12-13 20:17:44 +0700

    Edit landing page

--- src/components/cards/CardFood.jsx
+++ src/components/cards/CardFood.jsx
@@ -7,12 +7,12 @@ function CardFood({ id, name, description, fat, calorie, protein, carbohydrate, 
 			<div className="h-full flex flex-col justify-between">
 				<div>
 					<img
-						className="rounded-t-lg w-full max-h-[150px] sm:max-h-[150px] object-cover bg-slate-200 h-[150px]"
+						className="rounded-t-lg w-full max-h-[150px] sm:max-h-[120px] object-cover bg-slate-200 h-[150px]"
 						src={image || 'https://picsum.photos/265/150'}
 						alt={name}
 						loading="lazy"
 					/>
-					<div className="pt-5 pb-4 px-[14px] sm:px-[10px] ">
+					<div className="pt-5 sm:pt-2 pb-4 sm:pb-2 px-[14px] sm:px-[10px] ">
 						<h3 className="text-xl sm:text-base font-semibold text-navy">{name}</h3>
 						<div className="flex flex-col h-full justify-between">
 							<p className="text-sm sm:text-xs text-navy opacity-80">
@@ -23,17 +23,17 @@ function CardFood({ id, name, description, fat, calorie, protein, carbohydrate, 
 				</div>
 				<div className="px-2">
 					<div className="w-full grid grid-cols-4 gap pb-4">
-						<div className="text-xs flex justify-center gap-1 items-center text-navy sm:flex-col">
-							<img src="/icons/cloud-meatball-solid.svg" className="inline h-[18px] sm:h-4" alt="fat" /> {parseInt(fat)}
+						<div className="text-sm sm:text-[10px] flex justify-center gap-1 items-center text-navy">
+							<img src="/icons/cloud-meatball-solid.svg" className="inline h-[18px] sm:h-3" alt="fat" /> {parseInt(fat)}
 						</div>
-						<div className="text-xs  flex justify-center gap-1 items-center text-navy sm:flex-col">
-							<img src="/icons/fire-solid.svg" className="inline h-[18px] sm:h-4" alt="calorie" /> {parseInt(calorie)}
+						<div className="text-sm sm:text-[10px] flex justify-center gap-1 items-center text-navy">
+							<img src="/icons/fire-solid.svg" className="inline h-[18px] sm:h-3" alt="calorie" /> {parseInt(calorie)}
 						</div>
-						<div className="text-xs flex justify-center gap-1 items-center  text-navy sm:flex-col">
-							<img src="/icons/dna-solid.svg" className="inline h-[18px] sm:h-4" alt="protein" /> {parseInt(protein)}
+						<div className="text-sm sm:text-[10px] flex justify-center gap-1 items-center  text-navy">
+							<img src="/icons/dna-solid.svg" className="inline h-[18px] sm:h-3" alt="protein" /> {parseInt(protein)}
 						</div>
-						<div className="text-xs flex justify-center gap-1 items-center  text-navy sm:flex-col">
-							<img src="/icons/wheat-awn-solid.svg" className="inline h-[18px] sm:h-4" alt="carbohydrate" /> {parseInt(carbohydrate)}
+						<div className="text-sm sm:text-[10px] flex justify-center gap-1 items-center  text-navy">
+							<img src="/icons/wheat-awn-solid.svg" className="inline h-[18px] sm:h-3" alt="carbohydrate" /> {parseInt(carbohydrate)}
 						</div>
 					</div>
 				</div>
