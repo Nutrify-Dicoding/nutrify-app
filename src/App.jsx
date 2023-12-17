a25a0e246aa3cfeb60db39c011bcc6e4812f3a20
Ridho Ahmad Fauzi <ridho.ahmad21@students.unila.ac.id>
2023-12-17 17:39:27 +0700

    Edit footer and appbar before login

--- src/App.jsx
+++ src/App.jsx
@@ -40,91 +40,86 @@ function App() {
 				/>
 				<Route
 					path="/food-detail"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<FoodDetail />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/track"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<Tracking />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/alltrack"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<AllTrack />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/profile"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<Profile />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/profile/edit"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<EditProfile />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/profile/ubah-password"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<ChangePassword />
 						</Layout>
-    )}
+					}
 				/>
 				<Route
 					path="/foods/:food_id"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<FoodDetail />
 						</Layout>
-    )}
+					}
 				/>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route
 					path="/advice"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<Advice />
 						</Layout>
-    )}
-				/>
-				<Route
-					path="/favorite"
-					element={(
-      <Layout>
-							<Favorite />
-						</Layout>
-    )}
-				/>
-				<Route
-					path="/reset-password"
-					element={
-							<ForgetPassword />
 					}
 				/>
 				<Route
+					path="/favorite"
+					element={
+						<Layout>
+							<Favorite />
+						</Layout>
+					}
+				/>
+				<Route path="/reset-password" element={<ForgetPassword />} />
+				<Route
 					path="*"
-					element={(
-      <Layout>
+					element={
+						<Layout>
 							<NotFound />
 						</Layout>
-    )}
+					}
 				/>
 			</Routes>
 		</BrowserRouter>
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,34 +3,41 @@
 import { Link } from 'react-router-dom';
 
 function Footer() {
-    return (
-        <footer className="w-full flex flex-col bg-navy justify-between  items-center  ">
-            <div className="flex h-full w-full flex-wrap md:flex-col flex-row items-center justify-between py-[5rem] md:py-12 px-[6.25%]">
-                <img src="/icons/nutrify-logo-white.svg" alt="" />
-                <div className="text-white flex flex-row  gap-4 items-center justify-center md:mb-5 md:mt-4 ">
-                    <Link to="/">Home</Link>
-                    <span className="w-[6px] h-[6px] bg-white rounded-full" />
-                    <Link to="/favorite">Favorite</Link>
-                    <span className="w-[6px] h-[6px] bg-white rounded-full" />
-                    <Link to="/advice">Guide</Link>
-                </div>
-                <div className="flex flex-row items-center justify-center gap-3">
-                    <a href="#">
-                        <img src="/icons/facebook.svg" alt="" />
-                    </a>
-                    <a href="#">
-                        <img src="/icons/instagram.svg" alt="" />
-                    </a>
-                    <img src="/icons/x-twitter.svg" alt="" />
-                    <img src="/icons/envelope-regular.svg" alt="" />
-                </div>
-            </div>
-            <div className="text-sm w-full md:flex-col bg-darknavy flex flex-row justify-between px-[6.25%] py-[1.125rem] items-center text-white-400">
-                <p className="text-center opacity-80">2023@ All rights reserved. Design by Nutrify team’s </p>
-                <p className="opacity-80">Terms & Condition</p>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className="w-full flex flex-col bg-navy justify-between  items-center  ">
+			<div className="flex h-full w-full flex-wrap md:flex-col flex-row items-center justify-between py-[5rem] md:py-12 px-[6.25%]">
+				<img src="/icons/nutrify-logo-white.svg" alt="" />
+				<div className="text-white flex flex-row  gap-4 items-center justify-center md:mb-5 md:mt-4 ">
+					<Link to="/">Home</Link>
+					<span className="w-[6px] h-[6px] bg-white rounded-full" />
+					<Link to="/advice">Artikel</Link>
+					<span className="w-[6px] h-[6px] bg-white rounded-full" />
+					<a href="https://github.com/Nutrify-Dicoding" target="_blank">
+						Tentang Kami
+					</a>
+				</div>
+				<div className="flex flex-row items-center justify-center gap-3">
+					<a href="#">
+						<img src="/icons/facebook.svg" alt="" />
+					</a>
+					<a href="#">
+						<img src="/icons/instagram.svg" alt="" />
+					</a>
+					<img src="/icons/x-twitter.svg" alt="" />
+					<img src="/icons/envelope-regular.svg" alt="" />
+				</div>
+			</div>
+			<div className="text-sm w-full md:flex-col bg-darknavy flex flex-row justify-between px-[6.25%] py-[1.125rem] items-center text-white-400">
+				<p className="text-center opacity-80">2023@ All rights reserved. Design by Nutrify team’s </p>
+				<p className="opacity-80">
+					All data reserved from{' '}
+					<a href="https://spoonacular.com/" className="hover:underline">
+						Spoonacolar
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
